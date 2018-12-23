79be03d07494e88ee59ea8325cdc453113c48c41
Ícaro Falcão <vbsfalcao@gmail.com>
2018-12-23 08:44:40 -0300

    Create Login path and UserForLoginDto

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.DTOs;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
@@ -11,10 +17,12 @@ namespace DatingApp.API.Controllers
   public class AuthController : ControllerBase
   {
     private readonly IAuthRepository _repository;
+    private readonly IConfiguration _config;
 
-    public AuthController(IAuthRepository authRepository)
+    public AuthController(IAuthRepository authRepository, IConfiguration config)
     {
       _repository = authRepository;
+      _config = config;
     }
 
     [HttpPost("register")]
@@ -28,6 +36,32 @@ namespace DatingApp.API.Controllers
       };
       var createdUser = await _repository.Register(userToCreate, userForRegisterDto.Password);
       return StatusCode(201);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+    {
+      var userFromRepo = await _repository.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+      if (userFromRepo == null)
+        return Unauthorized();
+      var claims = new [] 
+      {
+        new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+        new Claim(ClaimTypes.Name, userFromRepo.Username)
+      };
+      var key = new SymmetricSecurityKey(Encoding.UTF8.
+        GetBytes(_config.GetSection("AppSettings:Token").Value));
+      var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+      var tokenDescriptor = new SecurityTokenDescriptor{
+        Subject = new ClaimsIdentity(claims),
+        Expires = DateTime.Now.AddDays(1),
+        SigningCredentials = credentials
+      };
+      var tokenHandler = new JwtSecurityTokenHandler();
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+      return Ok(new {
+        token = tokenHandler.WriteToken(token)
+      });
     }
   }
 }
